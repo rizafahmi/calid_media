5f66a1929f325fd9d3c6b744fd1bced1cc868dc7
Riza Fahmi <rizafahmi@gmail.com>
2016-01-17 17:09:49 +0700

    Add list link

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule CalidMedia.PageController do
   use CalidMedia.Web, :controller
 
   def index(conn, _params) do
-    media_dir = "/Users/riza/Movies/"
+    media_dir = "./priv/static/media"
     {:ok, files} = File.ls(media_dir)
     render conn, "index.html", files: files
   end
