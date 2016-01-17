c59efe73d93fe407db7bc19dba747dfeaa151dd1
Riza Fahmi <rizafahmi@gmail.com>
2016-01-17 17:33:46 +0700

    Add show routes

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -7,4 +7,8 @@ defmodule CalidMedia.PageController do
     filtered_files = Enum.filter(files, fn(file) -> String.ends_with?(file, [".mp4", ".mp3", ".avi"]) end)
     render conn, "index.html", files: filtered_files
   end
+
+  def show(conn, file) do
+    IO.inspect(file)
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule CalidMedia.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/show", PageController, :show
   end
 
   # Other scopes may use custom stacks.
