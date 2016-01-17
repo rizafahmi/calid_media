1baa7b21390ac226cadb3e2f7861e3562f937268
Riza Fahmi <rizafahmi@gmail.com>
2016-01-17 17:19:19 +0700

    Filter the file

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule CalidMedia.PageController do
   def index(conn, _params) do
     media_dir = "./priv/static/media"
     {:ok, files} = File.ls(media_dir)
-    render conn, "index.html", files: files
+    filtered_files = Enum.filter(files, fn(file) -> String.ends_with?(file, [".mp4", ".mp3", ".avi"]) end)
+    render conn, "index.html", files: filtered_files
   end
 end
