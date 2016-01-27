a265aacc4c757e614d8fdba727c63b21f2c8016f
Riza Fahmi <rizafahmi@gmail.com>
2016-01-27 19:23:34 +0700

    Add main menu

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,6 +3,7 @@ defmodule CalidMedia.PageController do
   alias CalidMedia.Media
 
   def index(conn, _params) do
+    keyword = ""
     cond do
       _params == %{} ->
         filter = [".mp4", ".mp3"]
@@ -10,14 +11,26 @@ defmodule CalidMedia.PageController do
         filter = [".mp4"]
       _params == %{"show" => "audio"} ->
         filter = [".mp3"]
+      %{"search" => keyword} = _params ->
+        filter = [".mp4", ".mp3"]
     end
 
     media_dir = "./priv/static/media"
     {:ok, files} = File.ls(media_dir)
     filtered_files = Enum.filter(files, fn(file) -> String.ends_with?(file, filter) end)
+
+    if keyword != "" do
+      filtered_files = Enum.filter(filtered_files, fn(file) ->
+        keyword
+        |> Regex.compile!("ui")
+        |> Regex.match?(file)
+      end)
+
+    end
+
     struct_files = Enum.map(filtered_files, fn(file) -> %CalidMedia.Media{filename: file} end )
 
-    render conn, "index.html", files: struct_files
+    render conn, "index.html", [files: struct_files, keyword: keyword]
   end
 
   def show(conn, %{"filename" => filename}) do
