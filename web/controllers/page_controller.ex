6d53ba9a87de6337f7262ea401d8d4c4422985e7
Riza Fahmi <rizafahmi@gmail.com>
2016-01-27 14:13:58 +0700

    Revamp the list page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,7 +5,7 @@ defmodule CalidMedia.PageController do
   def index(conn, _params) do
     media_dir = "./priv/static/media"
     {:ok, files} = File.ls(media_dir)
-    filtered_files = Enum.filter(files, fn(file) -> String.ends_with?(file, [".mp4", ".mp3", ".avi"]) end)
+    filtered_files = Enum.filter(files, fn(file) -> String.ends_with?(file, [".mp4", ".mp3"]) end)
     struct_files = Enum.map(filtered_files, fn(file) -> %CalidMedia.Media{filename: file} end )
     render conn, "index.html", files: struct_files
   end
